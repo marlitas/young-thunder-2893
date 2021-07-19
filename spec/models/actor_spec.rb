ba54d3ef139ec36805bea151ab0e607489a0010f
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-19 09:45:21 -0600

    Change describe 'actor' to Actor

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Actor' do
+RSpec.describe Actor do
   describe 'relationships' do
     it {should have_many :roles}
     it {should have_many(:movies).through(:roles)}

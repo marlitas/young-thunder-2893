47e45e30853d3af109472cb4fe5381d1ec630469
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-19 09:41:43 -0600

    Migrate to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_140134) do
+ActiveRecord::Schema.define(version: 2021_07_19_153742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -25,6 +30,13 @@ ActiveRecord::Schema.define(version: 2021_07_19_140134) do
     t.index ["studio_id"], name: "index_movies_on_studio_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "actor_id", null: false
+    t.index ["actor_id"], name: "index_roles_on_actor_id"
+    t.index ["movie_id"], name: "index_roles_on_movie_id"
+  end
+
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -33,4 +45,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_140134) do
   end
 
   add_foreign_key "movies", "studios"
+  add_foreign_key "roles", "actors"
+  add_foreign_key "roles", "movies"
 end

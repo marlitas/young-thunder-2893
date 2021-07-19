faf2c43b495d20a0d4fd9fc73b133db862281b1a
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-19 09:44:25 -0600

    Change describe roles to Role

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'Roles' do
+RSpec.describe Role do
   describe 'relationships' do
-    it {should belong_to :movie}
-    it {should belong_to :actor}
+    it {should belong_to(:movie)}
+    it {should belong_to(:actor)}
   end
 end
